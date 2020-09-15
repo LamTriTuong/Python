df6f7e6f508af36b90a7167777b05f5a6b95e19e
LamTriTuong <71242403+LamTriTuong@users.noreply.github.com>
2020-09-15 13:29:01 +0700

    Add files via upload

---
+++ Thongtinsinhvien.py
@@ -0,0 +1,11 @@
+
+                                             # Bài Tập 1
+                                         #Thông tin sinh viên
+name = "Lâm Trí Tường"
+age = 19
+MSSV = "1906020167"
+print("-----------THONG TIN SINH VIEN-----------")
+print(f"Hello thầy ! \nEm là: {name}\nTuổi: {age}\nMSSV: {MSSV}")
+
+                                                # Bài Tập 2
+                                    # Nhập 2 số nguyên in ra màn hình tổng bình phương
---
+++ lesson_6.py
@@ -0,0 +1,6 @@
+name ="Lâm Trí Tường"
+MSSV ="1906020167"
+age =19
+print(type(name))
+print(type(MSSV)) 
+print(type(age)) 
