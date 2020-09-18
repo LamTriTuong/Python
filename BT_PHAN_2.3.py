20b253609345d42f47a9126fd7338acac5b3073a
LamTriTuong <71242403+LamTriTuong@users.noreply.github.com>
2020-09-18 20:58:28 +0700

    Add files via upload

---
+++ BT_PHAN_2.3.py
@@ -0,0 +1,6 @@
+name = "Lâm Trí Tường"
+MSSV = "1906020167"
+khoa = "Công Nghệ Thông Tin"
+lop = "13THC"
+chuyen_nganh = "Công Nghệ Thông Tin"
+print(f"Em là : {name} \n MSSV : {MSSV} \n Khoa : {khoa}\n LOP : {lop}\n Chuyên ngành : {chuyen_nganh}")
