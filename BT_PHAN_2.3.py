2ad8dc7275883364409a8c42712de667cdcb1fbf
LamTriTuong <71242403+LamTriTuong@users.noreply.github.com>
2020-09-19 19:27:02 +0700

    Update BT_PHAN_2.3.py

--- BT_PHAN_2.3.py
+++ BT_PHAN_2.3.py
@@ -2,5 +2,5 @@ name = "Lâm Trí Tường"
 MSSV = "1906020167"
 khoa = "Công Nghệ Thông Tin"
 lop = "13THC"
-chuyen_nganh = "Công Nghệ Thông Tin"
+chuyen_nganh = "Lập Trình Ứng Dụng"
 print(f"Em là : {name} \n MSSV : {MSSV} \n Khoa : {khoa}\n LOP : {lop}\n Chuyên ngành : {chuyen_nganh}")
