a1eee286970c8a8fe6e0db481fb87bc85f89346e
LamTriTuong <71242403+LamTriTuong@users.noreply.github.com>
2020-09-19 19:28:11 +0700

    Update Thongtinsinhvien.py

--- Thongtinsinhvien.py
+++ Thongtinsinhvien.py
@@ -7,5 +7,4 @@ MSSV = "1906020167"
 print("-----------THONG TIN SINH VIEN-----------")
 print(f"Hello thầy ! \nEm là: {name}\nTuổi: {age}\nMSSV: {MSSV}")
 
-                                                # Bài Tập 2
-                                    # Nhập 2 số nguyên in ra màn hình tổng bình phương
+                                              
